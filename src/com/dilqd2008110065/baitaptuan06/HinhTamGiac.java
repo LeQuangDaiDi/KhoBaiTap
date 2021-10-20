226953563701a4b42532de5722b06fd6d17db15c
LEQUANGDAIDI <dile8861@gmail.com>
2021-10-20 19:39:07 +0700

    Bài Tập

---
+++ src/com/dilqd2008110065/baitaptuan06/HinhTamGiac.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class HinhTamGiac {
+    void AmThanh(){
+        System.out.println("è ... è ... è ...");
+        System.out.println("... ... ....");
+        System.out.println("... ... ....");
+
+    }
+    void TocDo(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập tốc độ xoay : ");
+        double tocdo = sc.nextDouble();
+
+    }
+    void Xoay(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập hệ số tọa độ ");
+        System.out.print("x = ");
+        double x = sc.nextDouble();
+        System.out.print("y = ");
+        double y = sc.nextDouble();
+        System.out.print("z = ");
+        double z = sc.nextDouble();
+        System.out.print("Nhập hệ tọa độ bạn muốn xoay ( x y z ):  ");
+        String goctoado = sc.next();
+        System.out.print("Nhập hệ số xoay  :  ");
+        Double hesotoado= sc.nextDouble();
+        TocDo();
+        AmThanh();
+        System.out.print("Xoay Thành Công");
+    }
+    
+}
+    
---
+++ src/com/dilqd2008110065/baitaptuan06/HoatDong.java
@@ -0,0 +1,12 @@
+public class HoatDong {
+    public static void main(String[] args) {
+        HinhTron ht = new HinhTron();
+        HinhVuong hv = new HinhVuong();
+        HinhTamGiac htg = new HinhTamGiac();
+        hv.Xoay();
+        ht.Xoay();
+        htg.Xoay();
+
+    }
+    
+}
